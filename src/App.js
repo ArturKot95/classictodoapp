988c4dc9b478b9c8606c7fd14c44e61b02f1e260
Artur <arturkot95@gmail.com>
2021-07-20 13:11:03 +0200

    remove redundant code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchTodos } from './features/todosSlice';
 import TodoList from './components/TodoList';
 
 import Navbar from './components/Navbar';
