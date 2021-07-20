cc0fa38f4d45f11cb8ded80699e20c236fb61f61
Artur <arturkot95@gmail.com>
2021-07-20 22:01:16 +0200

    add filter functionality

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,36 +1,52 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchTodos } from '../features/todosSlice';
 import TodoListItem from './TodoListItem';
 
 export default function TodoList(props) {
   let dispatch = useDispatch();
+  let [filterText, setFilterText] = useState('');
   let todos = useSelector(state => state.todos);
+  let filteredTodos = function() {
+    if (filterText.trim().length >= 3) {
+      return todos.filter(todo => todo.name.toLowerCase().includes(filterText.trim().toLowerCase()));
+    } else {
+      return todos;
+    }
+  }();
+
 
   useEffect(() => {
     dispatch(fetchTodos());
   }, [dispatch]);
 
-  let completedTodos = todos.filter(todo => todo.completed === true).map(todo => (
+  let completedTodos = filteredTodos.filter(todo => todo.completed === true).map(todo => (
     <TodoListItem key={todo._id} todo={todo} />
   ));
-  let uncompletedTodos = todos.filter(todo => todo.completed === false).map(todo => (
+  let uncompletedTodos = filteredTodos.filter(todo => todo.completed === false).map(todo => (
     <TodoListItem key={todo._id} todo={todo} />
   ));
 
-  return <ul className="list-group">
-    {todos.length > 0
-     ? <>
-      {uncompletedTodos}
-      {completedTodos.length > 0 && 
-      <>
-        <hr/>
-        <h4>Completed</h4>
-        {completedTodos}
-      </>}
-     </>
-     : <a role="button" className="primary-link" data-bs-toggle="modal" data-bs-target="#newTodoModal">Create one.</a>
+  return <>
+    <div className="form-floating mb-1">
+      <input type="text" className="form-control" placeholder="Filter..." 
+             onChange={(e) => setFilterText(e.target.value)} />
+      <label htmlFor="todoFilterInput">Filter...</label>
+    </div>
+    <ul className="list-group">
+      {todos.length > 0
+      ? <>
+        {uncompletedTodos}
+        {completedTodos.length > 0 && 
+        <>
+          <hr/>
+          <h4>Completed</h4>
+          {completedTodos}
+        </>}
+      </>
+      : <a role="button" className="primary-link" data-bs-toggle="modal" data-bs-target="#newTodoModal">Create one.</a>
 
-    }
-  </ul>
+      }
+    </ul>
+  </>
 }
