ebb2dc4851d4eadfe933d3f0a6da38192654f56c
Artur <arturkot95@gmail.com>
2021-07-21 15:19:19 +0200

    add backdrop fix

--- src/globals.js
+++ src/globals.js
@@ -1,3 +1,10 @@
 import React from 'react';
+import $ from 'jquery';
 
 window.React = React;
+
+// Bootstrap sometimes do not hide backdrop when modals/offcanvas are hiding programmatically
+window.backdropFix = function () {
+  $('body').css('overflow', 'auto');
+  $('.modal-backdrop').remove();
+}
