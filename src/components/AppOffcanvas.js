0428f6f14a7d59264fe52192239742ad34e9fad5
Artur <arturkot95@gmail.com>
2021-07-21 15:20:04 +0200

    app canvas

--- src/components/AppOffcanvas.js
+++ src/components/AppOffcanvas.js
@@ -1,19 +1,28 @@
 import { Offcanvas } from "bootstrap";
 import { useEffect, useRef } from "react";
+import $ from 'jquery';
 
 export default function AppOffcanvas(props) {
   let offcanvasRef = useRef(null);
+  let offcanvasInstance = useRef(null);
 
   useEffect(() => {
-    Offcanvas.getOrCreateInstance(offcanvasRef.current);
-  }, []);
+    offcanvasInstance.current = Offcanvas.getOrCreateInstance(offcanvasRef.current);
+    offcanvasInstance.current.show();
+    $('input', offcanvasRef.current)[0].focus();
+
+    offcanvasRef.current.addEventListener('hidden.bs.offcanvas', props.onHide);
+
+    return () => window.backdropFix();
+  }, [props]);
 
   return <div id={props.id} ref={offcanvasRef} className="offcanvas offcanvas-start">
     <div className="offcanvas-header">
       <h5 className="offcanvas-title">{props.title}</h5>
-      <div className="offcanvas-body">
-        {props.children}
-      </div>
+      <button className="btn-close text-reset" data-bs-dismiss="offcanvas"></button>
+    </div>
+    <div className="offcanvas-body">
+      { props.children }
     </div>
   </div>
 }
