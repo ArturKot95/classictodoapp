2be898badcb7534672c12d0f964d8704b1bea8b1
Artur <arturkot95@gmail.com>
2021-07-21 13:19:36 +0200

    add click event on todo item

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,24 +1,26 @@
-import { useEffect, useRef } from "react";
 import { useDispatch } from "react-redux"
 import { updateTodo } from "../features/todosSlice";
 import { addGlobalMessage } from '../features/toastSlice';
 
 export default function TodoListItem(props) {
   let dispatch = useDispatch();
-  let initialMount = useRef(true);
 
   function checkboxHandler(event, _id) {
     dispatch(() => {
       dispatch(updateTodo({_id, completed: event.target.checked}));
       dispatch(addGlobalMessage(`Todo ${props.todo.name} has been ${props.todo.completed ? '' : 'un'}done.`))
     });
+
+    event.stopPropagation();
   }
 
-  return <li className="list-group-item">
+  return <li className="list-group-item" role="button">
     <div className="d-flex">
       <div className="h5 mb-0 me-auto">{ props.todo.name }</div>
       <input type="checkbox" role="button" checked={ props.todo.completed }
-             className="form-check-input" onChange={(e) => checkboxHandler(e, props.todo._id)} />
+             className="form-check-input" 
+             onClick={(e) => e.stopPropagation()} 
+             onChange={(e) => checkboxHandler(e, props.todo._id)} />
     </div>
   </li>
 }
